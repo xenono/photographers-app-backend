be9557249cd88f0595ea1c459c762413487ffdb7
xenono <aurbanczyk0@gmail.com>
2020-11-09 12:30:28 +0000

    Addded process.env.PORT

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get("/", (req, res) => {
     res.send("Hello backend");
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("Server is running on http://localhost:8080");
 });
